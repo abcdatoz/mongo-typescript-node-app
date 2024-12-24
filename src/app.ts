5cf70c5b94017a2bc3249d76c6d9353aac0f20b8
rey <rhernandez@orfis.gob.mx>
2024-12-24 10:25:55 -0600

    lastCommit2024

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,7 @@
 import './bootstrap'
+import 'express-async-errors'
+import express, {Request, Response, NextFunction} from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import bodyParser from 'body-parser'
 
