c97da7faab78ee302704673dc990aaebde493d65
rey <rhernandez@orfis.gob.mx>
2025-07-08 17:00:25 -0600

    add password to cnnx

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,3 +1,6 @@
-const url = "mongodb://localhost:27017/pilot-ts";
+// const url = "mongodb://localhost:27017/pilot-ts";
+// export default url;
 
+
+const url = "mongodb://admin:orfisDev$13@localhost:27017/pilot-ts?authSource=admin";
 export default url;
